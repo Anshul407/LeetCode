82b5683a9b79ba7d00fd8050f924212b8595f957
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-01-31 11:29:10 +0530

    Time: 18 ms (42.77%), Space: 17.1 MB (22.86%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void mp(vector<int>nums,vector<int>op,vector<vector<int>>&ans){
+        if(nums.size()==0){
+            ans.push_back(op);
+            return ;
+        }
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            if(m[nums[i]]==0){
+                m[nums[i]]++;
+                vector<int>nums2(nums);
+                vector<int>op1(op);
+                op1.push_back(nums[i]);
+                nums2.erase(nums2.begin()+i);
+                mp(nums2,op1,ans);
+            }
+            else continue;
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>op;
+        mp(nums,op,ans);
+        return ans;
+    }
+};
