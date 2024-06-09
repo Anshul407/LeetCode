13f4d93cf9e85eedb6d2af8af3e751f6660445f5
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-06-09 18:46:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-    for (int i = 0; i <=30; i++)
-    {
-        if(pow(2,i)==n){
-            return true;
-        }
-        
-    }
-    return false;
+        if(n&(n-1)==0)return true;
+        return 0;
     }
 };
