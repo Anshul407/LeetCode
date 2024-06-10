33a4a0de7aac3ffa89cfacb59b08a441494b7844
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-06-10 16:06:21 +0530

    Time: 6 ms (71.08%), Space: 12.4 MB (60%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-
+        long long xorr=0;
+        for(auto i :nums){
+            xorr^=i;
         }
-        vector<int>a;
-        for(auto i:mp){
-            if(i.second==1){
-                a.push_back(i.first);
+        int rightmost=xorr^(xorr&(xorr-1));
+        int b1=0,b2=0;
+         for(auto i :nums){
+            if(rightmost&i)b1^=i;
+            else{
+                b2^=i;
             }
         }
-        return a;
+        return{b1,b2};
     }
 };
