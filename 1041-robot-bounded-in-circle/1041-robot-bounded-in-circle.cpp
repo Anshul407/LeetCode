1f38770e4b80ded508960a661a7396243055efd4
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-06-23 13:35:52 +0530

    Time: 3 ms (43.74%), Space: 7.3 MB (77.43%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    void turnleft(pair<pair<int,int>,char>&p){
+         if(p.second=='N')
+                p.second='W';
+                else if(p.second=='S'){
+                    p.second='E';
+                }
+                else if(p.second=='E'){
+                     p.second='N';
+                }
+                else{
+                   p.second='S';
+                }
+    }
+       void turnright(pair<pair<int,int>,char>&p){
+         if(p.second=='N')
+                p.second='E';
+                else if(p.second=='S'){
+                    p.second='W';
+                }
+                else if(p.second=='E'){
+                     p.second='S';
+                }
+                else{
+                   p.second='N';
+                }
+    }
+    bool isRobotBounded(string s) {
+        pair<pair<int,int>,char>p;
+        p.first.first=0;
+        p.first.second=0;
+        p.second='N';
+        for(auto i:s){
+            if(i=='G'){
+                if(p.second=='N')
+                p.first.second+=1;
+                else if(p.second=='S'){
+                    p.first.second-=1;
+                }
+                else if(p.second=='E'){
+                     p.first.first+=1;
+                }
+                else{
+                    p.first.first-=1;
+                }
+            }
+            if(i=='L'){
+                 turnleft(p);
+            }
+            if(i=='R'){
+                 turnright(p);
+            }
+        }
+        if(p.first.first==0&&p.first.second==0||p.second!='N')return 1;
+        return 0;
+    }
+};
