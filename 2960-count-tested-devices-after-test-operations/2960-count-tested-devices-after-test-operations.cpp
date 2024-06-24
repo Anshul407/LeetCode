07dd510b61a3792935d301110fe5dceac7490c8e
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-06-24 11:13:37 +0530

    Time: 0 ms (100%), Space: 20.2 MB (14.79%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int countTestedDevices(vector<int>& battery) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
         int cnt=0;
         for(int i=0;i<battery.size();i++){
            battery[i]= battery[i]-cnt;
