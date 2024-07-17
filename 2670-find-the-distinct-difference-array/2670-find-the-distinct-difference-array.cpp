b99d7dc2a5c98f0e65c99680aaa353d860333df4
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-07-17 10:51:17 +0530

    Time: 47 ms (57.11%), Space: 99.1 MB (52.62%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
@@ -9,12 +9,15 @@ public:
         for(auto i:nums){
             mp1[i]++;
             int flag=1e9;
+            int occ=1e9;
             if(mp2[i]>1){
+                occ=mp2[i];
+                occ--;
                 flag=i;
             }
             mp2.erase(i);
             if(flag!=1e9)
-            mp2[flag]++;
+            mp2[flag]=occ;
 
             cout<<mp2.size()<<" ";
             ans.push_back(mp1.size()-mp2.size());
