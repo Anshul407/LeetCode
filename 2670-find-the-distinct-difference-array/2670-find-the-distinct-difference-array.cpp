178d81b1c63e3c69425c3f5a3dbd14ec74eee739
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-07-17 10:52:21 +0530

    Time: 125 ms (5.24%), Space: 124.2 MB (5.98%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
@@ -1,27 +1,43 @@
-class Solution {
+  class Solution {
 public:
     vector<int> distinctDifferenceArray(vector<int>& nums) {
-        map<int,int>mp1,mp2;
-        for(auto i:nums){
-            mp2[i]++;
-        }
-        vector<int>ans;
-        for(auto i:nums){
-            mp1[i]++;
-            int flag=1e9;
-            int occ=1e9;
-            if(mp2[i]>1){
-                occ=mp2[i];
-                occ--;
-                flag=i;
-            }
-            mp2.erase(i);
-            if(flag!=1e9)
-            mp2[flag]=occ;
+        int ts = nums.size(); // Total size of the nums array
+        vector<int> ans;      // Answer vector to store the result
 
-            cout<<mp2.size()<<" ";
-            ans.push_back(mp1.size()-mp2.size());
+        // Step 1: Create a map to count the occurrences of each element in nums
+        map<int, int> m1;
+        for (const auto& i : nums) {
+            m1[i]++;
         }
+
+        // Step 2: Create a vector to store unique elements from nums
+        vector<int> uniq;
+        for (const auto& i : m1) {
+            uniq.push_back(i.first);
+        }
+
+        // Step 3: Iterate through nums to calculate prefix and suffix unique counts
+        for (int i = 0; i < ts; i++) {
+            map<int, int> prefix_map;
+            map<int, int> suffix_map;
+
+            // Calculate unique count in the prefix
+            for (int j = 0; j <= i; j++) {
+                prefix_map[nums[j]]++;
+            }
+            int ps = prefix_map.size();
+
+            // Calculate unique count in the suffix
+            for (int j = i + 1; j < ts; j++) {
+                suffix_map[nums[j]]++;
+            }
+            int ss = suffix_map.size();
+
+            // Difference between prefix and suffix unique counts
+            int as = ps - ss;
+            ans.push_back(as);
+        }
+
         return ans;
     }
 };
