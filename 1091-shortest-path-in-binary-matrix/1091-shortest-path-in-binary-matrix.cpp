e74b2e9672fed92553417ce248b6c4cd5f4c0273
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-07-19 18:48:57 +0530

    Time: 289 ms (5.07%), Space: 83 MB (5.03%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> path = {
+    vector<vector<int>> direction = {
     {1, 0},   // down
     {-1, 0},  // up
     {0, 1},   // right
@@ -17,39 +17,49 @@ bool issafe(int i, int j, int n, int m) {
 
 
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        queue<pair<int,int>>q;
-        int n=grid.size();
-        int m=grid[0].size();
-        if(grid.size()==1||grid[0].size()==1)return -1;
-        if(grid[0][0]==1)return -1;
+    typedef pair<int, pair<int, int>> P;
+    int m = grid.size();
+        int n = grid[0].size();
+        if(m == 0 || n == 0 || grid[0][0] != 0)
+            return -1;
 
-        q.push({0,0});
-        int step=1;
-        grid[0][0]=1;
-        while(!q.empty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                auto it=q.front();
-                q.pop();
-                int x=it.first;
-                int y=it.second;
-                if(x==n-1&&y==m-1)return step;
-                for(auto dir:path){
-                    int newx=x+dir[0];
-                    int newy=y+dir[1];
-                    if(issafe(newx,newy,n,m)&&grid[newx][newy]==0){
-                        grid[newx][newy]=1;
-                        q.push({newx,newy});
-                    }
+        auto isSafe = [&](int x, int y) {
+            return x>=0 && x<m && y>=0 && y<n;
+        };
+        
+        vector<vector<int>> result(m, vector<int>(n, INT_MAX));
+        priority_queue<P, vector<P>, greater<P>> pq;
+        
+        pq.push({0, {0, 0}});
+        result[0][0] = 0;
+
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+            int d=it.first;
+            int x=it.second.first;
+            int y=it.second.second;
+            for(auto dir:direction){
+                int newx=x+dir[0];
+                int newy=y+dir[1];
+                int dist=1;
+                if(isSafe(newx,newy) && grid[newx][newy] == 0 && d+dist < result[newx][newy]){
+                    grid[newx][newy]=1;
+                    pq.push({d+dist,{newx,newy}});
+                    result[newx][newy]=d+dist;
+
+                    
+
                 }
 
-               
             }
-             step++;
+
+        
         }
 
+        if(result[n-1][m-1]==INT_MAX)return -1;
 
-        return -1;
+        return result[n-1][m-1]+1;
         
     }
 };
