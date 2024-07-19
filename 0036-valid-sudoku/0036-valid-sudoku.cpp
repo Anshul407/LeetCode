c5c5aa76605c0179ad566a1368b1139c35c1d3c7
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-07-19 14:27:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool p(int x,int y,vector<vector<char>>& board,char val){
+        for(int i=0;i<board.size();i++){
+            cout<<board[i][y]<<" "<<board[x][i];
+            if(board[i][y]==val)return true;
+            if(board[x][i]==val)return true;
+            if(board[3*(x/3)+i/3][3*(y/3)+i%3]==val)return false;
+        }
+        return false;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board.size(); j++) {
+                char val = board[i][j];
+                if (val != '.' && val != ' ') {
+                    board[i][j] = '.'; // Temporarily mark the cell as empty
+                    if (p(i, j, board, val)) return false;
+                    board[i][j] = val; // Restore the original value
+                }
+            }
+        }
+        return true;
+    }
+};
