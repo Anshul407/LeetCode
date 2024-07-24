17769ee6ac1156ec873f2456d0e029e63508840e
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-07-24 22:38:58 +0530

    Time: 0 ms (100%), Space: 9.5 MB (12.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,11 +9,24 @@ public:
         return dp[i]=max(l,r);
     }
     int rob(vector<int>& nums) {
+        if(nums.size()==1)return nums[0];
+        if(nums.size()==2)return max(nums[0],nums[1]);
         int ans=0;
         int temp=0;
         int i=0;
-        vector<int>dp(nums.size()+1,-1);
-        return solve(nums,i,dp);
+        vector<int>dp(nums.size()+1,0);
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<=nums.size();i++){
+            dp[i]=max(dp[i-2]+nums[i-1],dp[i-1]);
+            cout<<dp[i]<<" ";
+        }
+      
+
+        for(auto i:dp){
+            ans=max(i,ans);
+        }
+        return ans;
        
     }
 };
