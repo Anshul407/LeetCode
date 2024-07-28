bd0d5bab780372ce4e14dbe982b28bc2cbfb4be6
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-07-28 14:41:23 +0530

    Time: 28 ms (15.84%), Space: 21.5 MB (8.32%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -9,6 +9,22 @@ public:
             }
         }
     }
+    void bfs(unordered_map<int,vector<int>>&mp,int u,vector<int>&vis){
+        queue<int>q;
+        q.push(u);
+        vis[u]=true;
+
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            for(auto v:mp[it]){
+                if(!vis[v]){
+                    q.push(v);
+                    vis[v]=1;
+                }
+            }
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         unordered_map<int,vector<int>>mp;
         for(int i=0;i<isConnected.size();i++){
@@ -24,7 +40,7 @@ public:
         vector<int>vis(V,false);
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                dfs(mp,i,vis);
+                bfs(mp,i,vis);
                 ans++;
             }
         }
