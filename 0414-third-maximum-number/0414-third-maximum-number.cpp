335635ec8e323e91204418e4ed20bdd095838a82
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-08-08 16:08:29 +0530

    Time: 3 ms (84.77%), Space: 11.7 MB (67.32%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-       set<int>st(nums.begin(),nums.end());
-       vector<int>v;
-       for(auto i :st){
-        v.push_back(i);
-       }
-       if(v.size()==1){
-        return v[0];
-       }
-       if(v.size()==2){
-        return v[1];
-       }
-       return v[v.size()-3];
+        long long first = LLONG_MIN;
+        long long second = LLONG_MIN;
+        long long third = LLONG_MIN;
+        int f=0,ff=0;
         
+        for(int i=0;i<nums.size();i++){
+            if(first<nums[i]){
+                third=second;
+                second=first;
+                first=nums[i];
+                
+            }
+
+            if(second<nums[i] && first!=nums[i]){
+               
+                third=second;
+                second=nums[i];
+                
+            }
+            if(third<=nums[i] && second!=nums[i]&&first!=nums[i]){
+                third=nums[i];
+                f=1;
+            }
+        }
+        
+        if(third==LLONG_MIN&&!f)return first;
+        
+        
+
+        return third;
     }
 };
