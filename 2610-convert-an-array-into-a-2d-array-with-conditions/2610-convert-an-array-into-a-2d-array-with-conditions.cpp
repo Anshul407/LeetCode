7f1de9869663f3c94733c7b75466c0497ba91a06
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-08-11 23:02:51 +0530

    Time: 13 ms (34.94%), Space: 34 MB (55.7%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<vector<int>>ans;
+        map<int,int>mp;
+        for(auto i:nums){
+            mp[i]++;
+        }
+        while(true){
+            vector<int>x;
+            for(auto &i:mp){
+                if(i.second){
+                x.push_back(i.first);
+                i.second--;
+                
+                }
+
+                
+            }
+            if(!x.empty())ans.push_back(x);
+            else return ans;
+
+        }
+        return ans;
+    }
+};
