08f1e3103e01359bd9a1b89109fd96d2dddabfdb
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-08-11 15:03:54 +0530

    Time: 145 ms (95.47%), Space: 46.5 MB (63.97%) - LeetHub

---
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    const int MOD=1e9+7;
+    int solve(vector<vector<int>>&grid,int i,int j,vector<vector<int>>&dp){
+        if(dp[i][j]!=-1)return dp[i][j];
+        
+        int count=1;
+        
+        if(i>0){
+            if(grid[i-1][j]>grid[i][j]){
+                count=(count+solve(grid,i-1,j,dp))%MOD;
+            }
+        }
+        if(j>0){
+            if(grid[i][j-1]>grid[i][j]){
+                count=(count+solve(grid,i,j-1,dp))%MOD;
+            }
+        }
+        if(i<grid.size()-1){
+            if(grid[i+1][j]>grid[i][j]){
+                count=(count+solve(grid,i+1,j,dp))%MOD;
+            }
+        }
+        if(j<grid[0].size()-1){
+            if(grid[i][j+1]>grid[i][j]){
+                count=(count+solve(grid,i,j+1,dp))%MOD;
+            }
+        }
+
+        return dp[i][j]=count%MOD;
+    }
+    int countPaths(vector<vector<int>>& grid) {
+        vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),-1));
+        int x=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                x=(x+solve(grid,i,j,dp))%MOD;
+            }
+        }
+        return x%MOD;
+    }
+};
