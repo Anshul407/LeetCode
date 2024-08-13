115c29c4a70f2936ab41c34629074b1259ea6aa2
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-08-13 14:11:33 +0530

    Time: 0 ms (100%), Space: 8.3 MB (12.05%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,55 +1,14 @@
 class Solution {
 public:
-    int strStr(string h, string n) {
-       int ans = -1;
-    int cnt = 0;
-    int j = n.length() - 1;
+    int strStr(string haystack, string needle) {
+        int m=needle.size();
+        int n=haystack.size();
+        if(n<m)return -1;
+        // if(n==m)return 0;
+        for(int i=0;i<=haystack.size()-m;i++){
+            if(haystack.substr(i,m)==needle)return i;
+        }
+        return -1;
+    }
 
-    for (int i = h.length() - 1; i >= 0; i--) {
-        if (j>=0&&h[i] == n[j]) {
-            cnt++;
-            j--;
-        } else {
-            cnt = 0;
-            j = n.length() - 1;
-            if (j>=0&&h[i] == n[j]) {
-            cnt++;
-            j--;
-        }
-        }
-        if (cnt == n.length()) {
-            ans = i;
-            cnt = 0;
-            j = n.length() - 1;
-            if (j>=0&&h[i] == n[j]) {
-            cnt++;
-            j--;
-        }
-            
-            
-        }
-    }
-    cnt=0;
-    j=0;
-   
-        for(int i=0;i<h.length();i++){
-                 
-                   if(h[i]==n[j]){
-                       cnt++;
-                       j++;
-                   }
-                   else{
-                       cnt=0;
-                       j=0;
-                        if (j>=0&&h[i] == n[j]) {
-            cnt++;
-            j++;
-        }
-                   }
-                     if(cnt==n.length()){
-                       ans=i-n.length()+1;
-                       break;
-                   }
-        }return ans;
-    }
 };
