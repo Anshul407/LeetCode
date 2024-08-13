e66e612f015a7af9c81e8365abf7e93035f5b034
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-08-13 23:01:12 +0530

    Time: 862 ms (27.14%), Space: 219.5 MB (5.07%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void dfs(int node,map<int,vector<int>>&mp,vector<bool>&vis){
+    void dfs(int node,unordered_map<int,vector<int>>&mp,vector<bool>&vis){
         vis[node]=1;
         for(auto i:mp[node]){
             if(!vis[i]){
@@ -10,7 +10,7 @@ public:
     }
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
         vector<bool>vis(n,0);
-        map<int,vector<int>>mp;
+        unordered_map<int,vector<int>>mp;
         for(auto i:edges){
             mp[i[0]].push_back(i[1]);
             mp[i[1]].push_back(i[0]);
