04063f5194ef90ec1c89ffd9cc9c731390d884df
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-08-15 15:04:53 +0530

    Time: 205 ms (11.31%), Space: 62 MB (35.46%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
-    void dfs(int i,vector<int>&ans,map<int,vector<int>>&mp,int c){
-        ans[i]=c;
-        for(auto v:mp[i]){
-            if(!ans[v]){
-                dfs(v,ans,mp,c+1);
+    void dfs(int i,vector<int>&ans,map<int,vector<int>>&mp){
+       vector<bool> used(5, false); // Track used colors (1 to 4)
+
+        // Mark the colors used by the neighboring gardens
+        for (auto v : mp[i]) {
+            if (ans[v]) {
+                used[ans[v]] = true;
             }
         }
+
+        // Assign the first available color
+        for (int c = 1; c <= 4; ++c) {
+            if (!used[c]) {
+                ans[i] = c;
+                break;
+            }
+        }
+
+        // Recursively color the adjacent nodes
+        for (auto v : mp[i]) {
+            if (!ans[v]) {
+                dfs(v, ans, mp);
+            }
+        }
+        
     }
     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
         map<int,vector<int>>mp;
@@ -17,7 +35,8 @@ public:
         vector<int>ans(n+1,0);
         for(int i=1;i<=n;i++){
             if(ans[i]==0){
-                dfs(i,ans,mp,1);
+                
+                dfs(i,ans,mp);
             }
         }
         ans.erase(ans.begin());
