deb2bb9e2aa4876b05f7fb4096cac50b0bef5ba9
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-08-17 15:23:38 +0530

    Time: 6 ms (63.09%), Space: 18.2 MB (7.76%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans=0;
+        for(auto i:operations){
+            if(i[0]=='+'||i[1]=='+')ans++;
+            else ans--;
+        }
+        return ans;
+    }
+};
