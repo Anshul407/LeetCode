3f510dfaca724f0fcffd154448b80235c04c7c68
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-08-21 12:33:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
+    bool dfs(int i,int p,vector<int>&vis,unordered_map<int,vector<int>>mp,vector<int>&ans){
+        vis[i]=1;
+        for(auto v:mp[i]){
+            if(vis[v]==0){
+                if(dfs(v,i,vis,mp,ans))return 1;
+            }
+            else if(i==p)return 1;
+        }
+        ans.push_back(i);
+        return 0;
+    }
     vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
         vector<int>ans;
         vector<int>ind(n,0);
-        unordered_map<int,vector<int>>mp;
+       unordered_map<int,vector<int>>mp;
        for(auto i:prerequisites){
             mp[i[1]].push_back(i[0]);
        }
        for(int i=0;i<n;i++){
-        for(auto v:mp[i]){
-            ind[v]++;
-        }
-       }
-       queue<int>q;
-       for(int i=0;i<n;i++){
-        if(ind[i]==0)q.push(i);
-       }
-       while(!q.empty()){
-        int u=q.front();
-        ans.push_back(u);
-        q.pop();
-            for(auto v:mp[u]){
-                ind[v]--;
-                if(ind[v]==0)q.push(v);
-            }
+            if(ind[i]==0)
+           if (dfs(i,-1,ind,mp,ans))return{};
        }
        if(ans.size()!=n)return {};
+       reverse(ans.begin(),ans.end());
        return ans;
     }
 };
