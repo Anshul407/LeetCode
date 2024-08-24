215fa80f6f6a095799adb0ca36cf614a0bfe473e
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-08-24 19:00:14 +0530

    Time: 3 ms (36.65%), Space: 7.4 MB (63.35%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
+    int dp[1001];
     int solve(int n,int i,int s){
         if(i>n)return 0;
         if(n==i)return 1;
         if(s>=n)return 0;
+        if(dp[i]!=-1)return dp[i];
         int tk=1;
         int ntk=1;
         if(s+i<=n){
@@ -11,11 +13,12 @@ public:
         }
         ntk=solve(n,i,s+1);
 
-        return max(tk,ntk);
+        return dp[i]= max(tk,ntk);
 
         
     }
     int integerBreak(int n) {
+        memset(dp,-1,sizeof(dp));
         return solve(n,0,1);
     }
 };
