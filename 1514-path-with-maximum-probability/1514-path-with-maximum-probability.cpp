d42edaed28d976457887b2f9d9dc62ce5597e3e0
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-08-31 22:19:25 +0530

    Time: 190 ms (6.56%), Space: 79.8 MB (5.04%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-  
-   
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        map<int, vector<pair<int, double>>> mp;
-        for (int j = 0; j < edges.size(); ++j) {
-            mp[edges[j][0]].emplace_back(edges[j][1], succProb[j]);
-            mp[edges[j][1]].emplace_back(edges[j][0], succProb[j]);
+        map<int,vector<pair<int,double>>>mp;
+        int j=0;
+        for(auto i:edges){
+            mp[i[0]].push_back({i[1],succProb[j]});
+            mp[i[1]].push_back({i[0],succProb[j]});
+            j++;  
         }
 
-        priority_queue<pair<double,int>,vector<pair<double,int>>>q;
-        vector<double>ans(n,0.0);
-
-        q.push({0,start_node});
-        vector<bool>vis(n,0);
-        vis[start_node]=1;
-        while(!q.empty()){
-            auto node=q.top().second;
-            auto p=q.top().first;
-            q.pop();
-            if(p==0)p=1;
-            if(node==end_node)return p;
+        priority_queue<pair<double,int>>pq;
+        pq.push({1.0,start_node});
+        vector<double>res(n,0.0);
+        res[start_node]=0;
+        while(!pq.empty()){
+            auto wt=pq.top().first;
+            auto node=pq.top().second;
+            pq.pop();
+            if(node==end_node)return wt;
             for(auto v:mp[node]){
-                if(p*v.first>ans[v.second]&&!vis[v.second]){
-                    vis[v.second]=1;
-                    v.second=p*v.first;
-                    q.push({p*v.first,v.second});
+                auto cn=v.first;
+                auto cw=v.second;
+                if(cw*wt>res[cn]){
+                    res[cn]=cw*wt;
+                    pq.push({cw*wt,cn});
                 }
             }
         }
-        
         return 0.0;
     }
 };
