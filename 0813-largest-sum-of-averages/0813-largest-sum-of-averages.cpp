d785a68f16aa0d2b600d106feff31d84de4b1721
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-09-01 21:16:02 +0530

    Time: 7 ms (65.06%), Space: 9.7 MB (82.27%) - LeetHub

--- 0813-largest-sum-of-averages/0813-largest-sum-of-averages.cpp
+++ 0813-largest-sum-of-averages/0813-largest-sum-of-averages.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+    vector<vector<double>>dp;
     double solve(vector<int>&nums,int k,int i)
     {   
         if(k>0&&i>=nums.size())return INT_MIN;
         if(k==0&&i>=nums.size())return 0;
         if(k<=0&&i!=nums.size())return INT_MIN;
+        if(dp[i][k]!=-1)return dp[i][k];
         double tk=0;
         double m=INT_MIN;
         for(int j=i;j<nums.size();j++){
@@ -17,9 +19,10 @@ public:
             }
             
         }
-        return m;
+        return dp[i][k]= m;
     }
     double largestSumOfAverages(vector<int>& nums, int k) {
+        dp.resize(nums.size(),vector<double>(k+1,-1));
        return solve(nums,k,0);
     }
 };
