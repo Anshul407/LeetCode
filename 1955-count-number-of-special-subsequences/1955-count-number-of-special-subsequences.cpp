1a7e3bb09b8ee05751a9420e17a5cd7d38929834
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-09-10 22:12:53 +0530

    Time: 153 ms (66.27%), Space: 174.8 MB (92.46%) - LeetHub

---
+++ 1955-count-number-of-special-subsequences/1955-count-number-of-special-subsequences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+
+    int countSpecialSubsequences(vector<int>& nums) {
+       
+        long dp0 = 0, dp1 = 0, dp2 = 0;
+        
+        for (int num : nums) {
+            if (num == 0) {
+                dp0 = (2 * dp0 + 1) % MOD; 
+            } else if (num == 1) {
+                dp1 = (2 * dp1 + dp0) % MOD; 
+            } else if (num >= 2) {
+                dp2 = (2 * dp2 + dp1) % MOD; 
+            }
+        }
+        
+        return dp2 % MOD;
+    }
+};
