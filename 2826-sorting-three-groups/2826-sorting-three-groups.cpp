7ba2b0c6c3da6c2a7ab5840b19aba985c810f25c
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-09-15 17:31:48 +0530

    Time: 88 ms (12.22%), Space: 98.9 MB (25.43%) - LeetHub

--- 2826-sorting-three-groups/2826-sorting-three-groups.cpp
+++ 2826-sorting-three-groups/2826-sorting-three-groups.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
+    vector<vector<int>>dp;
     int solve(vector<int>&nums,int i,int cur){
         if(i==nums.size())return 0;
-
+        if(dp[i][cur]!=-1)return dp[i][cur];
         int op1=INT_MAX,op2=INT_MAX,op3=INT_MAX;
         if(nums[i]==cur){
             op1=solve(nums,i+1,cur);
@@ -15,9 +16,10 @@ public:
             op1=1+solve(nums,i+1,cur);
             op2=solve(nums,i+1,nums[i]);
         }
-        return min(op1,op2);
+        return dp[i][cur]= min(op1,op2);
     }
     int minimumOperations(vector<int>& nums) {
+        dp.resize(nums.size(),vector<int>(4,-1));
         return solve(nums,0,0);
     }
 };
