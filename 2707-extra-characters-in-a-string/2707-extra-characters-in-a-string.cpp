4bc24df4eb6eb6d722c4b2c49da9ceee99054928
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-09-23 13:08:18 +0530

    Time: 211 ms (28.04%), Space: 76.2 MB (64.02%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
+    int dp[100001];
     map<string,int>mp;
     int solve(string s,int i){
         if(i>s.length())return 0;
-
+        if(dp[i]!=-1)return dp[i];
         int ntk=1+solve(s,i+1);
         int op2=INT_MAX;
         string cur="";
         for(int j=i;j<s.length();j++){
             cur+=s[j];
-            if(mp.find(cur)!=mp.end())op2=solve(s,j+1);
+            if(mp.find(cur)!=mp.end())op2=min(op2,solve(s,j+1));
         }
 
-        return min(op2,ntk);
+        return dp[i]= min(op2,ntk);
     }
     int minExtraChar(string s, vector<string>& dictionary) {
+        memset(dp,-1,sizeof(dp));
         for(auto i:dictionary){
             mp[i]++;
         }
