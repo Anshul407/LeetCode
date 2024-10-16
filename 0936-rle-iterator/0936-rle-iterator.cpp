89e6a0521ed62651eef3775e95697f2c5f653078
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-10-16 19:14:12 +0530

    Time: 0 ms (100%), Space: 13.4 MB (32.31%) - LeetHub

---
+++ 0936-rle-iterator/0936-rle-iterator.cpp
@@ -0,0 +1,40 @@
+class RLEIterator {
+public:
+    vector<pair<int,int>>mp;
+    
+    RLEIterator(vector<int>& encoding) {
+        for(int i=0;i<encoding.size()-1;i+=2){
+            if(encoding[i]>0)
+            mp.push_back({encoding[i+1],encoding[i]});
+        }
+    }
+    
+    int next(int n) {
+        // for(auto i:mp)cout<<i.first<<" "<<i.second<<" ";
+        // cout<<endl;
+        for(auto &i:mp){
+           
+            if(i.second==0)continue;
+            if(i.second>=n){
+                i.second-=n;
+                return i.first;
+            }
+        //    else if(i.second==n){
+        //         i.second-=n;
+        //         n=0;
+        //     }
+            else{
+                
+                n-=i.second;
+                i.second=0;
+            }
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your RLEIterator object will be instantiated and called as such:
+ * RLEIterator* obj = new RLEIterator(encoding);
+ * int param_1 = obj->next(n);
+ */
