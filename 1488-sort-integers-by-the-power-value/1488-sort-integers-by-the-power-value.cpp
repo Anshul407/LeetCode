6891a9901ded7445d69cabcfb4ebe62956d53074
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-10-19 16:17:14 +0530

    Time: 47 ms (44.19%), Space: 19.2 MB (38.55%) - LeetHub

---
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(int x){
+        if(x<0)return 1e9;
+        if(x==1)return 0;
+
+        int op1=INT_MAX,op2=INT_MAX;
+        if(x%2==0)op1=1+solve(x/2);
+        else op2=1+solve(3*x+1);
+        return min(op1,op2);
+    }
+    int getKth(int lo, int hi, int k) {
+        vector<vector<int>>ans;
+        for(int i=lo;i<=hi;i++){
+            ans.push_back({solve(i),i});
+          
+        }
+        sort(ans.begin(),ans.end());
+        return ans[k-1][1];
+    }
+};
