c6666daed37aa88ee1eb763585dc9f3a39b6e3e8
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-10-21 17:44:13 +0530

    Time: 15 ms (15.88%), Space: 23.5 MB (14.81%) - LeetHub

---
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int n;
+    string solve(map<string,int>mp,string &temp){
+        if(temp.size()==n){
+            if(mp.find(temp)==mp.end())return temp;
+            return "";
+        }
+        temp.push_back('0');
+        string op1=solve(mp,temp);
+        if(op1.size()==n)return op1;
+        temp.pop_back();
+        temp.push_back('1');
+        string op2=solve(mp,temp);
+        temp.pop_back();
+        return op2;
+
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        map<string,int>mp;
+        n=nums[0].size();
+        for(auto i:nums){
+            mp[i]++;
+        }
+        string temp="";
+       return solve(mp,temp);
+    }
+};
