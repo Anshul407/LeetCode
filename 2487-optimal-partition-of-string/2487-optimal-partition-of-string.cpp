0799797307aebc9913d88081ef873144e73cbe49
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-10-25 23:29:22 +0530

    Time: 189 ms (9.96%), Space: 75.8 MB (5.98%) - LeetHub

---
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        map<int,int>mp;
+        int cnt=1;
+        for(int i=0;i<s.length();i++){
+            if(mp.find(s[i])!=mp.end()){
+                cnt++;
+                mp.clear();
+            }
+            mp[s[i]]++;
+        }
+        
+        return cnt;
+    }
+};
