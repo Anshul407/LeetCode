36c9e92d49a6a68bafcc9189c17813830515c38c
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-10-29 14:28:40 +0530

    Time: 27 ms (5.09%), Space: 29.3 MB (5.25%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int dp[101][201];
-    int solve(string &s, int i, int op) {
-        if(i==s.length())return op==0;
-        if(dp[i][op]!=-1)return dp[i][op];
+    int solve(string s,int i,int c){
+        if(i>=s.size())return c==0;
+        if(dp[i][c]!=-1)return dp[i][c];
         int ans=0;
         if(s[i]=='*'){
-            ans=max(ans,solve(s,i+1,op+1));
-            ans=max(ans,solve(s,i+1,op));
-            if(op)
-            ans=max(ans,solve(s,i+1,op-1));
-        }else {
-            if(s[i]=='(')ans=solve(s,i+1,op+1);
-            else if(op) ans=solve(s,i+1,op-1);
+            ans=max(ans,solve(s,i+1,c));
+            ans=max(ans,solve(s,i+1,c+1));
+            if(c)ans=max(ans,solve(s,i+1,c-1));
+        }
+        else {
+            if(s[i]=='(')ans=max(ans,solve(s,i+1,c+1));
+            else if(c) ans=max(ans,solve(s,i+1,c-1));
         }
 
-        return dp[i][op]= ans;
+        return dp[i][c]=ans;
+        
     }
-
     bool checkValidString(string s) {
         memset(dp,-1,sizeof(dp));
-       
-        return solve(s, 0, 0);
+       return solve(s,0,0);
     }
 };
