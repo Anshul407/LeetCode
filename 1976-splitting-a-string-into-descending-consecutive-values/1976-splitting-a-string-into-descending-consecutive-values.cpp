e001e69cf0bc9aa0ae49a1686416df0b62074330
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-10-29 15:42:21 +0530

    Time: 7 ms (5.88%), Space: 10.7 MB (13.89%) - LeetHub

---
+++ 1976-splitting-a-string-into-descending-consecutive-values/1976-splitting-a-string-into-descending-consecutive-values.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool solve(string s,int i,int cnt,int size,long long p){
+        if(i>=s.size())return cnt>=2;
+        for(int j=i;j<s.size();j++){
+            auto st=s.substr(i,j-i+1);
+            if(st.size()>19||(st.size()==19&&st>"9223372036854775807"))break;
+            auto x=stoll(s.substr(i,j-i+1));
+            if(p==-1||(x==p-1)){
+                cout<<x<<" ";
+                if(solve(s,j+1,cnt+1,size+(j-i+1),x))return 1;
+
+            }
+        }
+        return 0;
+    }
+    bool splitString(string s) {
+        
+        return solve(s,0,0,0,-1);
+    }
+};
