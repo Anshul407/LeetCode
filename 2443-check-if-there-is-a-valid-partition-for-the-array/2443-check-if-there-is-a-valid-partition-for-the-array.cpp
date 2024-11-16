e313d4519145b0c992ecfbea4ce4dfb610698daf
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-11-16 18:52:36 +0530

    Time: 12 ms (45.4%), Space: 92.9 MB (51.17%) - LeetHub

--- 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -6,18 +6,14 @@ public:
         if(dp[i]!=-1)return dp[i];
         int op1=0,op2=0,op3=0;
         if(i+1<nums.size()&&nums[i]==nums[i+1]){
-            op1=max(op1,solve(nums,i+2));
-            
+            op1=max(op1,solve(nums,i+2));  
         }
         if(i+2<nums.size()&&nums[i+2]==nums[i+1]&&nums[i]==nums[i+1]){
                 op2=max(op2,solve(nums,i+3));
-                
-
         }
         if(i+2<nums.size()){
                 if(nums[i]==nums[i+1]-1&&nums[i+1]==nums[i+2]-1)
-                op3=max(op3,solve(nums,i+3));
-                  
+                op3=max(op3,solve(nums,i+3));          
         }
         return dp[i]=op1||op2||op3;
     }
