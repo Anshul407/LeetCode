2c219a8ded6abd1fd06bc0a772c72bd2a8e47b64
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-11-24 18:58:23 +0530

    Time: 0 ms (100%), Space: 20.8 MB (11.39%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,47 +1,30 @@
 class Solution {
 public:
-    void dfs(unordered_map<int,vector<int>>&mp,int u,vector<int>&vis){
-        if(vis[u])return ;
-        vis[u]=true;
-        for(auto v:mp[u]){
+    void solve(int i,vector<int>&vis,map<int,vector<int>>&mp){
+        vis[i]=1;
+        for(auto v:mp[i]){
             if(!vis[v]){
-                dfs(mp,v,vis);
-            }
-        }
-    }
-    void bfs(unordered_map<int,vector<int>>&mp,int u,vector<int>&vis){
-        queue<int>q;
-        q.push(u);
-        vis[u]=true;
-
-        while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            for(auto v:mp[it]){
-                if(!vis[v]){
-                    q.push(v);
-                    vis[v]=1;
-                }
+                solve(v,vis,mp);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        unordered_map<int,vector<int>>mp;
+        map<int,vector<int>>mp;
+        int j=0;
         for(int i=0;i<isConnected.size();i++){
-            for(int j=0;j<isConnected[0].size();j++){
+            for(int j=0;j<isConnected.size();j++){
                 if(isConnected[i][j]){
                     mp[i].push_back(j);
                     mp[j].push_back(i);
                 }
             }
         }
-        int V=mp.size();
         int ans=0;
-        vector<int>vis(V,false);
-        for(int i=0;i<V;i++){
+        vector<int>vis(isConnected.size(),0);
+        for(int i=0;i<isConnected.size();i++){
             if(!vis[i]){
-                bfs(mp,i,vis);
                 ans++;
+                solve(i,vis,mp);
             }
         }
         return ans;
