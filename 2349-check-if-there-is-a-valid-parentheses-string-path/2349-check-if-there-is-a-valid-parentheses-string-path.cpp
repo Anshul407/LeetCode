af7ee0ddcc80a890f7c30d1db2c9829180ce26c9
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-11-30 19:49:00 +0530

    Time: 71 ms (78.26%), Space: 26.1 MB (69.78%) - LeetHub

--- 2349-check-if-there-is-a-valid-parentheses-string-path/2349-check-if-there-is-a-valid-parentheses-string-path.cpp
+++ 2349-check-if-there-is-a-valid-parentheses-string-path/2349-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int solve(vector<vector<char>>& g,int i,int j,int op, vector<vector<vector<int>>>&dp){
+    int dp[101][101][202];
+    int solve(vector<vector<char>>& g,int i,int j,int op){
         if(op<0||i>=g.size()||j>=g[0].size()||g[i][j]=='c')return 0;
         if(i==g.size()-1&&j==g[0].size()-1){
             if(g[i][j]=='(')return 0;
@@ -8,14 +9,14 @@ public:
         }
         if(dp[i][j][op]!=-1)return dp[i][j][op];
         
-        int op1=solve(g,i+1,j,g[i][j]=='('?op+1:op-1,dp);
+        int op1=solve(g,i+1,j,g[i][j]=='('?op+1:op-1);
         if(op1)return dp[i][j][op+1]=1;
-        int op2=solve(g,i,j+1,g[i][j]=='('?op+1:op-1,dp);
+        int op2=solve(g,i,j+1,g[i][j]=='('?op+1:op-1);
         if(op2)return dp[i][j][op]=1;
         return dp[i][j][op]= op1||op2;
     }
     bool hasValidPath(vector<vector<char>>& grid) {
-        vector<vector<vector<int>>>dp(grid.size(), vector<vector<int>>(grid[0].size(), vector<int>(((grid.size() + grid[0].size())+ 2), -1)));
-        return solve(grid,0,0,0,dp);
+        memset(dp,-1,sizeof(dp));
+        return solve(grid,0,0,0);
     }
 };
