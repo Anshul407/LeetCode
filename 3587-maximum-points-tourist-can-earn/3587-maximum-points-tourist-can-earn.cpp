59755dd06a3b0e71c0f711289400029bcef4b551
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-12-02 17:50:00 +0530

    Time: 427 ms (80.21%), Space: 57.1 MB (83.06%) - LeetHub

---
+++ 3587-maximum-points-tourist-can-earn/3587-maximum-points-tourist-can-earn.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[201][201];
+    int solve(int i,int d,int n,int k,vector<vector<int>>& stayScore, vector<vector<int>>& travelScore){
+        if(d>=k)return 0;
+        if(dp[i][d]!=-1)return dp[i][d];
+        int ans=0;
+        for(int j=0;j<n;j++){
+            if(j==i){
+                ans=max(ans,stayScore[d][i]+solve(i,d+1,n,k,stayScore,travelScore));
+            }
+            else{
+                ans=max(ans,travelScore[i][j]+solve(j,d+1,n,k,stayScore,travelScore));
+            }
+        }
+        return dp[i][d]=ans;
+    }
+    int maxScore(int n, int k, vector<vector<int>>& stayScore, vector<vector<int>>& travelScore) {
+        memset(dp,-1,sizeof(dp));
+        int ans=0;
+        for(int i=0;i<n;i++){
+
+        
+        ans=max(ans, solve(i,0,n,k,stayScore,travelScore));}
+        return ans;
+    }
+};
