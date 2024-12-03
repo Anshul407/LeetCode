1f1b800a89c21a68fd347829c69a6a47ed4b8489
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-12-03 12:20:55 +0530

    Time: 24 ms (5.72%), Space: 38.9 MB (5.15%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool dfs(int i,map<int,vector<int>>&mp,int p,int a,int b,vector<int>&vis){
+        vis[i]=1;
+        for(auto v:mp[i]){
+           
+            if(v==p)continue;
+            if(vis[v])return 1;
+            if(dfs(v,mp,i,a,b,vis))return 1;
+        }
+        return 0;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        map<int,vector<int>>mp;
+        for(auto i:edges){
+            mp[i[0]].push_back(i[1]);
+            mp[i[1]].push_back(i[0]);
+            int u=i[0];
+            int v=i[1];
+            vector<int>vis(edges.size()+1,0);
+            if(dfs(u,mp,-1,u,v,vis)){
+                return {u,v};
+            }
+        }
+        return {};
+    }
+};
