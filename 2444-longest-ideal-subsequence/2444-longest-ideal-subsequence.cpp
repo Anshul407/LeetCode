e22d3c03efa12ff4ada08cd2836ec110ae0a87bb
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-12-09 12:22:36 +0530

    Time: 523 ms (9.47%), Space: 194.7 MB (8.42%) - LeetHub

--- 2444-longest-ideal-subsequence/2444-longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/2444-longest-ideal-subsequence.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    int solve(string &s, int i, int k, int p, vector<vector<int>> &dp) {
-        if (i >= s.size()) return 0;
-        
-        int prevIndex = (p == -1 ? 26 : s[p] - 'a');
-        if (dp[i][prevIndex] != -1) return dp[i][prevIndex];
-
-        // Include current character if valid
-        int op1 = 0, op2 = 0;
-        if (p == -1 || abs(s[p] - s[i]) <= k)
-            op1 = 1 + solve(s, i + 1, k, i, dp);
-
-        // Skip current character
-        op2 = solve(s, i + 1, k, p, dp);
-
-        return dp[i][prevIndex] = max(op1, op2);
+    
+    int solve(string &s,int i,int k,int p,vector<vector<int>>&dp){
+        if(i>=s.size())return 0;
+        int key=(p==-1?26:s[p]-'a');
+        if(dp[i][key]!=-1)return dp[i][key];
+        int op1=0,op2=0;
+        if(p==-1||abs(s[p]-s[i])<=k)op1=1+solve(s,i+1,k,i,dp);
+        op2=solve(s,i+1,k,p,dp);
+        return dp[i][key]=max(op1,op2);
     }
-
     int longestIdealString(string s, int k) {
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(27, -1));
-        return solve(s, 0, k, -1, dp);
+        vector<vector<int>>dp(s.size(),vector<int>(27,-1));
+        return solve(s,0,k,-1,dp);
     }
 };
