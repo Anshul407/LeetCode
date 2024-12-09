5215d55e267bc407b9e9eaafd3178d194a31c969
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-12-09 17:33:43 +0530

    Time: 0 ms (100%), Space: 16.1 MB (92.49%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,52 +1,73 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int vis[n][m];
-        int cntf=0;
-        int cnt=0;
-        queue<pair<pair<int,int>,int>>q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    vis[i][j]=2;
-                }
-                else{
-                  vis[i][j]=0;
-                }
-                if(grid[i][j]==1){
-                    cntf++;
-
+        queue<pair<int, int>> q;
+        int n = grid.size(), m = grid[0].size();
+        int x = 0, y = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
                 }
             }
         }
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
-int tm=0;
-        while(!q.empty()){
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int t=q.front().second;
-            tm=max(t,tm);
-            q.pop();
 
-            for(int i=0;i<4;i++){
-                int rw=r+dr[i];
-                int cl=c+dc[i];
-                if(rw>=0&&cl>=0&&rw<n&&cl<m&&vis[rw][cl]==0&&grid[rw][cl]==1){
-                    q.push({{rw,cl},t+1});
-                    vis[rw][cl]=2;
-                    cnt++;
+        int ans = 0;
+        while (!q.empty()) {
+            int size = q.size();
+            int flag = 0;
+            for (int mm = 0; mm < size; mm++) {
+                auto it = q.front();
+                q.pop();
+                int i = it.first;
+                int j = it.second;
+
+                if (i > 0) {
+                    int up = i - 1;
+                    if (grid[up][j] == 1) {
+                        q.push({up, j});
+                        grid[up][j] = 0;
+                        flag = 1;
+                    }
+                }
+                if (j > 0) {
+                    int up = j - 1;
+                    if (grid[i][up] == 1) {
+                        q.push({i, up});
+                        grid[i][up] = 0;
+                        flag = 1;
+                    }
+                }
+                if (i < n - 1) {
+                    int up = i + 1;
+                    if (grid[up][j] == 1) {
+                        q.push({up, j});
+                        grid[up][j] = 0;
+                        flag = 1;
+                    }
+                }
+                if (j < m - 1) {
+                    int up = j + 1;
+                    if (grid[i][up] == 1) {
+                        q.push({i, up});
+                        grid[i][up] = 0;
+                        flag = 1;
+                    }
                 }
             }
-            
-
+              if (flag)
+            ans++;
         }
-        if(cnt!=cntf)return -1;
-            
-            return tm;
-        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return ans;
+
+      
     }
+    
 };
