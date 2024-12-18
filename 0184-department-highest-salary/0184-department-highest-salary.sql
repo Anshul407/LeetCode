40b0f0de1c9dbb80e5b2abb28ac11bcdc57852c5
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-12-18 14:01:16 +0530

    Time: 1572 ms (6.2%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,4 @@
+-- with b as 
+-- (select Department.name as Department,Employee.name as Employee,Employee.salary as Salary from Department join Employee on Department.id=Employee.departmentId)
+-- select * from b where Salary in (select max(Salary),Department from b group by Department);
+select Department.name as Department,Employee.name as Employee,Employee.salary as Salary from Department join Employee on Department.id=Employee.departmentId  where salary=(select max(salary) from Employee where Department.id=Employee.departmentId)
