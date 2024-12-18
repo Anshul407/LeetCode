e741421e658d6437f776fab90f4b49e37f6f4d66
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-12-18 10:59:53 +0530

    Time: 27 ms (52.45%), Space: 17.5 MB (61.71%) - LeetHub

--- 3239-minimum-number-of-operations-to-make-x-and-y-equal/3239-minimum-number-of-operations-to-make-x-and-y-equal.cpp
+++ 3239-minimum-number-of-operations-to-make-x-and-y-equal/3239-minimum-number-of-operations-to-make-x-and-y-equal.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    
-    int minimumOperationsToMakeEqual(int x, int y) {
-        queue<pair<int,int>>q;
-        q.push({x,0});
-        unordered_set<int>vis;
-        vis.insert(x);
-        while(!q.empty()){
-            auto node=q.front().first;
-            auto w=q.front().second;
-            q.pop();
-            if(node==y)return w;
-            if(node%11==0&&vis.find(node/11)==vis.end()){
-                q.push({node/11,w+1});
-                vis.insert(node/11);
-            }
-            if(node%5==0&&vis.find(node/5)==vis.end()){
-                q.push({node/5,w+1});
-                vis.insert(node/5);
-            }
-            if(vis.find(node-1)==vis.end()){
-                q.push({node-1,w+1});
-                vis.insert(node-1);
-            }
-            if(vis.find(node+1)==vis.end()){
-                q.push({node+1,w+1});
-                vis.insert(node+1);
-            }
+    unordered_map<int,int>mp;
+    int solve(int x,int y){
+        if(x==y)return 0;
+        if(x<y)return (y-x);
+        if(mp.find(x)!=mp.end())return mp[x];
+        int op1=1e6,op2=1e6;
+        int op3=1e6,op6=1e6;
+        int op4=1e6,op5=1e6;
+        int op7=1e6;
+        if(x%11==0){
+            op1=1+solve(x/11,y);
+        }else{
+            int tot=x%11;
+            op1=min(op1,1+solve(x/11,y)+tot);
+            op1=min(op1,1+solve(x/11+1,y)+(11-tot));
         }
-        return -1;
+        if(x%5==0){
+            op4=1+solve(x/5,y);
+        }else{
+            int tot=x%5;
+            op4=min(op4,1+solve(x/5,y)+tot);
+            op4=min(op4,1+solve(x/5+1,y)+(5-tot));
+        }
+        op7=1+solve(x-1,y);
+
+        return mp[x]= min({op1,op4,op7});
+    }
+    int minimumOperationsToMakeEqual(int x, int y) {
+        mp.clear();
+        return solve(x,y);
     }
 };
