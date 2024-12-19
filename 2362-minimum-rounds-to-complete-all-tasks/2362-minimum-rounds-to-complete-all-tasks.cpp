8c0bd5b624fc67c13e8473d1edeba8c0d0149d03
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2024-12-19 19:15:31 +0530

    Time: 31 ms (74.39%), Space: 107.6 MB (35.37%) - LeetHub

---
+++ 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    //prioriy queue kyu use kia hai?
+    //mereko ab pta chl gya ki utna jarrori nahi tha pehle 3 pairs ko remove krna
+    //hata du tera kuch code??
+    //tikh hain
+    
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int,int> mp;
+        for(auto it:tasks)
+        {
+            mp[it]++;
+        }
+        // priority_queue<pair<int,int>> pq;
+        // for (auto [n,c]:mp) {
+        // pq.push({c,n});
+        // }
+    //    int count=0;
+    //    // for(auto v:mp)
+   //     // {   count++;
+        //     auto [c,n]=pq.top();
+        //     pq.pop();
+        //     if(c%3==0||c%3==2)
+        //     {
+        //         c-=3;
+        //     }
+        //     else if(c%2==0)
+        //     {
+        //         c-=2;
+        //     }
+        //     else{
+        //         return -1;
+        //     }
+        //     if(c!=0)
+        //     {
+        //         pq.push({c,n});
+        //     }
+        // }
+        // return count;
+        int ans=0;
+        for(auto v:mp){
+            auto nodeocc=v.second;
+            if(nodeocc==1)return -1;
+            if(nodeocc%3==0){
+                ans+=nodeocc/3;
+            }
+            else{
+               ans+=(nodeocc/3);
+               ans++;
+            }
+            //ruk jaaa
+
+        }
+        return ans; //main kuch improvis e kru??ans;
+
+    }
+};
