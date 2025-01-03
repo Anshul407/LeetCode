a9a7aa97a0e0279be4140f0d285a8ba204f8e49b
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2025-01-03 11:11:11 +0530

    Time: 0 ms (100%), Space: 26.1 MB (23.34%) - LeetHub

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
@@ -11,42 +11,27 @@
  */
 class Solution {
 public:
- 
-    void solve(TreeNode*root,int val,int depth,int cd){
-        if(root==NULL)return ;
-     
-      
-        if(depth-1==cd){
-            if(root->left){
+    TreeNode*solve(TreeNode*root,int val,int d){
+        if(!root)return NULL;
+        root->left=solve(root->left,val,d-1);
+        root->right=solve(root->right,val,d-1);
+        if(d==1){
                 TreeNode*temp=root->left;
-             
                 root->left=new TreeNode(val);
-                
                 root->left->left=temp;
-            }
-            if(root->right){
-                TreeNode*temp=root->right;
-              
+                temp=root->right;
                 root->right=new TreeNode(val);;
                 root->right->right=temp;
         }
-        return ;
-
-            
-        }
-        solve(root->left,val,depth,cd+1);
-        solve(root->right,val,depth,cd+1);
-        
-       
-        
+        return root;
     }
     TreeNode* addOneRow(TreeNode* root, int val, int depth) {
-        if (depth == 1) {
+        depth--;
+        if(depth==0){
             TreeNode* newRoot = new TreeNode(val);
             newRoot->left = root;
             return newRoot;
         }
-        solve(root,val,depth,1);
-        return root;
+        return solve(root,val,depth);
     }
 };
