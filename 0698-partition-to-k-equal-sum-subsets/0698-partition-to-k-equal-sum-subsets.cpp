99be02b8fbb2699f736bb2cd51e89c16f29d258f
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2025-01-10 15:42:12 +0530

    Time: 21 ms (73.91%), Space: 30.2 MB (13.77%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
-    int f;
+    int val;
     vector<int>dp;
-    int solve(vector<int>&nums,int i,int cur,int k,int mask){
-        if(k==0)return mask==(1<<nums.size())-1;
-        if(cur==f)return solve(nums,0,0,k-1,mask);
-        if(i>=nums.size()||cur>f)return 0;
-        if(dp[mask]!=-1)return dp[mask];
-        for(int x=0;x<nums.size();x++){
-            if((mask&(1<<x))==0&&cur+nums[x]<=f){
-                int newmask=mask|(1<<x);
-                if(solve(nums,x+1,cur+nums[x],k,newmask))return dp[mask]=1;   
-            }
-        } 
-        return dp[mask]= 0;
+    bool solve(vector<int>& nums,int sum,int bitmask,int k)
+    {
+        if(sum==0&&k==1)
+        return true;
+        if(sum<0)return false;
+        if(dp[bitmask]!=-1)return dp[bitmask];
+        if(sum==0)
+        {
+            k--;
+            sum=val;
+        }
+        int ans=false;
+        for(int i=0;i<nums.size();i++)
+        {
+            if((bitmask&(1<<i))==false)
+            ans=ans||solve(nums,sum-nums[i],bitmask|(1<<i),k);
+        }
+        return dp[bitmask]= ans;
     }
     bool canPartitionKSubsets(vector<int>& nums, int k) {
+        dp.resize(1<<nums.size(),-1);
         int sum=0;
-        for(auto i:nums){
-            sum+=i;
-        }
-        if(sum%k!=0)return 0;
-        f=(sum/k);
-        dp.resize((1<<nums.size())+1,-1);
-        return solve(nums,0,0,k,0);
-        
+        for(int i=0;i<nums.size();i++)
+        {sum+=nums[i];}
+        if(sum%k!=0)
+        return false;
+        sum/=k;
+        for(auto it:nums)
+        if(it>sum)
+        return false;
+        val=sum;
+        return solve(nums,sum,0,k);
+
     }
 };
