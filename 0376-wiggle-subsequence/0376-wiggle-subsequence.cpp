824c201677cf068c829324d0ae2ab999d9428872
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2025-01-14 14:06:46 +0530

    Time: 33 ms (8.36%), Space: 23.3 MB (9.08%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    int dp[1001][1001][2];
-    int solve(vector<int>&nums,int i,int diff,int p,int cnt){
+    int dp[1001][1001][3];
+    int solve(vector<int>&nums,int i,int p,int flag){
         if(i>=nums.size())return 0;
-        int k=diff>0;
-        if(dp[i][cnt][k]!=-1)return dp[i][cnt][k];
-        int ntk=solve(nums,i+1,diff,p,cnt);
-        int tk=0;
-        if(cnt==0){
-            
-            tk=1+solve(nums,i+1,diff,i,cnt+1);
+        int op1=0,op2=0;
+        if(dp[i][p+1][flag+1]!=-1)return dp[i][p+1][flag+1];
+        if(p==-1){
+            op1=1+solve(nums,i+1,i,-1);
+        }else if(flag==-1){
+            int newflag=(nums[i]-nums[p])>0;
+            if(nums[i]-nums[p]!=0)
+            op1=1+solve(nums,i+1,i,newflag);
+        }else if(((nums[i]-nums[p])>0)!=flag){
+            if(nums[i]-nums[p]!=0)
+            op1=1+solve(nums,i+1,i,(nums[i]-nums[p])>0);
         }
-        else if(cnt==1){
-            diff=nums[i]-nums[p];
-            if(diff<0||diff>0){
-                tk=1+solve(nums,i+1,diff,i,cnt+1);
-            }
-
-        }
-        else {
-            if(diff<0&&nums[i]-nums[p]>0){
-                tk=1+solve(nums,i+1,nums[i]-nums[p],i,cnt+1);
-            }
-            else if(diff>0&&nums[i]-nums[p]<0){
-                tk=1+solve(nums,i+1,nums[i]-nums[p],i,cnt+1);
-            }
-
-        }
-        return dp[i][cnt][k]= max(tk,ntk);
+        op2=solve(nums,i+1,p,flag);
+        return dp[i][p+1][flag+1]=max(op1,op2);
     }
     int wiggleMaxLength(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
-        return solve(nums,0,0,-1,0);
+        return solve(nums,0,-1,-1);
     }
 };
