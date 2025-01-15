111726d727e78259f98b88f4d47e883814abb928
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2025-01-15 10:20:00 +0530

    Time: 0 ms (100%), Space: 8.3 MB (5.14%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    int minimizeXor(int num1, int num2) {
+        string a,b;
+        bitset<31>binary(num1);
+        a=binary.to_string();
+        cout<<a;
+        int tot=__builtin_popcount(num2);
+        for(int i=0;i<a.size();i++){
+            if(a[i]=='1'){
+                if(tot){
+                    tot--;
+                    b+='1';
+                }else b+='0';
+            }else {
+                b+='0';
+            }
+        }
+        if(tot){
+            for(int i=b.size()-1;i>=0;i--){
+                if(b[i]=='0'){
+                    if(tot){
+                        tot--;
+                        b[i]='1';
+                    }
+                    if(!tot)break;
+                }
+            }
+        }
+        int x=stoi(b,0,2);
+        return x;
+
+    }
+};
