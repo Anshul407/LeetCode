bacfea6e80ccb7d43ffc4aa9f222ca3e2e02f641
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2025-01-16 11:26:49 +0530

    Time: 0 ms (100%), Space: 64.3 MB (64.63%) - LeetHub

---
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        //xor associative law 
+       if(nums1.size()%2==0&&nums2.size()%2==0)return 0;
+       if(nums1.size()%2!=0&&nums2.size()%2!=0){
+        int cur=0;
+        for(auto i:nums1)cur^=i;
+        for(auto i:nums2)cur^=i;
+        return cur;
+       }
+       int cur=0;
+       if(nums1.size()%2==0){
+        for(auto i:nums1)cur^=i;
+       }
+       if(nums2.size()%2==0){
+        for(auto i:nums2)cur^=i;
+       }
+       return cur;
+    }
+};
