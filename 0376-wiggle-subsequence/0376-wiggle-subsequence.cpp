b941e38d93cef7c818add9146bbb5f5f990a8c3d
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2025-01-22 23:15:36 +0530

    Time: 38 ms (7.89%), Space: 23.2 MB (8.97%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int dp[1001][1001][3];
-    int solve(vector<int>&nums,int i,int p,int flag){
+    int dp[1001][1002][3];
+    int solve(vector<int>&nums,int i,int j,int f){
         if(i>=nums.size())return 0;
-        int op1=0,op2=0;
-        if(dp[i][p+1][flag+1]!=-1)return dp[i][p+1][flag+1];
-        if(p==-1){
+        if(dp[i][j+1][f+1]!=-1)return dp[i][j+1][f+1];
+        int op1=0;
+        if(j==-1){
             op1=1+solve(nums,i+1,i,-1);
-        }else if(flag==-1){
-            int newflag=(nums[i]-nums[p])>0;
-            if(nums[i]-nums[p]!=0)
-            op1=1+solve(nums,i+1,i,newflag);
-        }else if(((nums[i]-nums[p])>0)!=flag){
-            if(nums[i]-nums[p]!=0)
-            op1=1+solve(nums,i+1,i,(nums[i]-nums[p])>0);
+        }else if(f==-1){
+            if(nums[i]-nums[j]!=0)
+            op1=1+solve(nums,i+1,i,nums[i]-nums[j]>0?1:0);
+        }else {
+            if(f==1&&nums[i]-nums[j]<0){
+                op1=1+solve(nums,i+1,i,0);
+            }else if(f==0&&nums[i]-nums[j]>0)op1=1+solve(nums,i+1,i,1);
         }
-        op2=solve(nums,i+1,p,flag);
-        return dp[i][p+1][flag+1]=max(op1,op2);
+        int op2=solve(nums,i+1,j,f);
+        return dp[i][j+1][f+1]=max(op1,op2);
     }
     int wiggleMaxLength(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
