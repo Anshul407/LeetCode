4694b3119baf520fef098c6102df61886a27ecc4
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2025-01-23 11:01:33 +0530

    Time: 0 ms (100%), Space: 26.6 MB (59.58%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -1,56 +1,54 @@
 class Solution {
 public:
-    vector<int>parent,rank;
+    vector<int>rank,parent;
     void abc(int n){
-        parent.resize(n+1);
         rank.resize(n+1,1);
+        parent.resize(n+1,0);
         for(int i=0;i<=n;i++)parent[i]=i;
     }
-    int findp(int n){
-        if(n==parent[n])return n;
-        return parent[n]=findp(parent[n]);
+    int find(int cur){
+        if(parent[cur]==cur)return cur;
+
+        return parent[cur]=find(parent[cur]);
     }
-    void unionbyrank(int u,int v){
-        int pu=findp(u);
-        int pv=findp(v);
-        if(pu==pv)return ;
+    void unionbyrank(int a,int b){
+        int pa=find(a);
+        int pb=find(b);
+        if(pa==pb)return ;
 
-        if(rank[pu]>rank[pv])parent[pv]=pu;
-        else if(rank[pu]<rank[pv])parent[pu]=pv;
-        else {
-            parent[pu]=pv;
-            rank[pv]++;
-
+        if(rank[pa]>rank[pb]){
+            parent[pb]=pa;
+        }else if(rank[pa]<rank[pb]){
+            parent[pa]=pb;
+        }else{
+            parent[pb]=pa;
+            rank[pa]++;
         }
+        
     }
     int countServers(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int ans=0;
-        abc(n+m+1);
-         map<int,int>mp;
+        int n=grid.size()+grid[0].size();
+        abc(n+1);
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==1){
                     int u=i;
-                    int v=j+n;
+                    int v=j+grid.size();
                     unionbyrank(u,v);
-                    
                 }
             }
         }
-     
+        int ans=0;
+        map<int,int>mp;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==1){
-                    mp[findp(i)]++;
+                    mp[find(i)]++;
                 }
             }
         }
         for(auto i:mp){
             if(i.second>1)ans+=i.second;
-        }
-        return ans;
-        
+        }return ans;    
     }
 };
