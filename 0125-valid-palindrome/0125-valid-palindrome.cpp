3f562a3d407ba11d27ba5958126443bd001df1bd
Anshul407 <kunwaranshul7739@gmail.com>
2025-01-25 18:20:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int solve(string s,int i,int j){
+        if(i>=j)return 1;
+        if(isalnum(s[i])&&isalnum(s[j])){
+            if(tolower(s[i])!=tolower(s[j]))return 0;
+            return solve(s,i+1,j-1);
+        }
+        if(!isalnum(s[i])&&!isalnum(s[j]))return solve(s,i+1,j-1);
+        if(!isalnum(s[i]))return solve(s,i+1,j);
+        return solve(s,i,j-1);
+    }
+    bool isPalindrome(string s) {
+        return solve(s,0,s.size()-1);
+    }
+};
