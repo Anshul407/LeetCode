5e551f29a7ff099df788bdce4bd2ab03828901de
Anshul407 <kunwaranshul7739@gmail.com>
2025-01-29 11:44:37 +0530

    Time: 0 ms (100%), Space: 8.5 MB (55.29%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int ans=0;
+        stack<char>st;
+        for(auto i:s){
+            if(i=='('){
+                st.push(i);
+            }else{
+                if(st.empty())ans++;
+                else st.pop();
+            }
+        }
+        return ans+st.size();
+    }
+};
