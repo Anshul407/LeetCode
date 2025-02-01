1570a744057dc5f8a7e0f9fd2efc0257b50531a4
Anshul407 <kunwaranshul7739@gmail.com>
2025-02-01 18:18:51 +0530

    Time: 3 ms (19.48%), Space: 10.5 MB (5.1%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
+    unordered_set<int>vis;
     bool isHappy(int n) {
-        map<int,int>mp;
-        mp[1]=1;
-        for(int i=2;i<9;i++)mp[i]=0;
-        mp[7]=1;
-
-        while(n>=10){
-            string x=to_string(n);
-            int c=0;
-            for(auto i:x){
-                int cc=i-'0';
-                c+=(cc*cc);
-            }
-            n=c;
-        }
-        return mp[n];
+        if(n==1)return 1;
+        if(vis.find(n)!=vis.end())return 0;
+        vis.insert(n);
+        auto x=to_string(n);
+        int p=0;
+        for(auto i:x){
+            auto k=i-'0';
+            p+=(k*k);
+        }return isHappy(p);
     }
 };
