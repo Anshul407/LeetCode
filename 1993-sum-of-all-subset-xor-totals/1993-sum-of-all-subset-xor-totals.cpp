7f029fdb466c69c840ea2c2d957f1ff3f6d414b0
Anshul407 <kunwaranshul7739@gmail.com>
2025-04-09 11:00:12 +0530

    Time: 0 ms (100%), Space: 9.2 MB (65.38%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int solve(vector<int>&num,int i,int cur){
-        if(i>=num.size())return cur;
-
-        int op=0;
-        op+=solve(num,i+1,cur^num[i]);
-        op+=solve(num,i+1,cur);
-
-        return op;
+    int fnc(vector<int>& nums,int i,int ans)
+    {
+        if(i>=nums.size())
+        return ans;
+        int op1=0,op2=0;
+        op1+=fnc(nums,i+1,(ans^nums[i]));
+        op1+=fnc(nums,i+1,ans);
+        return op1;
     }
     int subsetXORSum(vector<int>& nums) {
-        return solve(nums,0,0);
+        return fnc(nums,0,0);
     }
 };
