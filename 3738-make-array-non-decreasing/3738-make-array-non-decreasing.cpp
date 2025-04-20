7f65931fc0e08beb17d7be5760846bf6235c0b3b
Anshul407 <kunwaranshul7739@gmail.com>
2025-04-20 11:07:56 +0530

    Time: 3 ms (69.86%), Space: 206.1 MB (99.12%) - LeetHub

--- 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
+++ 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     int maximumPossibleSize(vector<int>& nums) {
         int cur=-1;
-        int ind=-1;
         int ans=0;
         for(int i=0;i<nums.size();i++){
             if(cur==-1){
-                ind=i;
                 cur=nums[i];
                 ans++;
             }
             else{
                 if(cur<=nums[i]){
-                    ind=i;
                     cur=nums[i];
                     ans++;
                 }
