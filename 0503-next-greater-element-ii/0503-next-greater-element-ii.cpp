15f687c7a93589b569439ae7a56b01f569cad73d
Anshul407 <kunwaranshul7739@gmail.com>
2025-05-15 00:04:34 +0530

    Time: 2 ms (80.29%), Space: 28.6 MB (38.34%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        stack<int>st;
-        map<int,int>mp;
-        int i=0;
-        int f=0;
-        while(i<nums.size()*2){
-            while(st.size()&&nums[st.top()]<nums[i%nums.size()]){
-                if(mp.find(st.top())==mp.end())
-                mp[st.top()]=nums[i%nums.size()];
+        vector<int>ans(nums.size(),-1);
+        stack<pair<int,int>>st;
+        st.push({0,nums[0]});
+        for(int i=1;i<nums.size();i++){
+            while(!st.empty()&&st.top().second<nums[i]){
+                ans[st.top().first]=nums[i];
+                st.pop();
+            }st.push({i,nums[i]});
+        }
+        if(st.empty())return ans;
+        for(int i=0;i<nums.size();i++){
+            if(i>=st.top().first)break;
+            while(!st.empty()&&st.top().second<nums[i]){
+                ans[st.top().first]=nums[i];
                 st.pop();
             }
-            if(i<nums.size())
-            st.push(i);
-            i++;
+
         }
-        
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            if(mp.find(i)!=mp.end())ans.push_back(mp[i]);
-            else ans.push_back(-1);
-        }return ans;
+        return ans;
     }
 };
