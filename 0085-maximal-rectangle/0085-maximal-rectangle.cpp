73b779b38d00a5049b26470f51011869c1209922
Anshul407 <kunwaranshul7739@gmail.com>
2025-05-15 16:13:07 +0530

    Time: 18 ms (17.75%), Space: 19.4 MB (42.29%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    int solve(vector<int>& cur) {
-        stack<int> st;
-        int ans = 0;
-        for (int i = 0; i <= cur.size(); i++) {
-            while (!st.empty() && (i == cur.size() || cur[st.top()] >= (i == cur.size() ? 0 : cur[i]))) {
-                int height = cur[st.top()];
+    int solve(vector<int>&cur){
+        stack<int>st;
+        int ans=0;
+        for(int i=0;i<=cur.size();i++){
+            while(!st.empty()&&(i==cur.size()||cur[st.top()]>=cur[i])){
+                auto height=cur[st.top()];
                 st.pop();
-                int width = st.empty() ? i : i - st.top() - 1;
-                ans = max(ans, width * height);
-            }
-            st.push(i);
+                auto width=0;
+                if(st.empty())width=i;
+                else{
+                    width=i-st.top()-1;
+                }
+                ans=max(ans,width*height);
+            }st.push(i);
         }
         return ans;
     }
-
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty()) return 0;
-        int ans = 0;
-        int n = matrix.size(), m = matrix[0].size();
-
-        for (int i = 0; i < n; i++) {
-            vector<int> cur(m, 0);
-            for (int j = 0; j < m; j++) {
-                int k = i, cnt = 0;
-                while (k >= 0) {
-                    if (matrix[k][j] == '0') break;
-                    cnt++;
-                    k--;
-                }
-                cur[j] = cnt;
+        int ans=0;
+        for(int i=0;i<matrix.size();i++){
+        vector<int>cur;
+        for(int j=0;j<matrix[0].size();j++){
+            int k=i;
+            int cnt=0;
+            while(k>=0){
+                if(matrix[k][j]=='0')break;
+                cnt++;
+                k--;
             }
-            ans = max(ans, solve(cur));
+            cur.push_back(cnt);
         }
-        return ans;
+        ans=max(ans,solve(cur));
+        }return ans;
     }
 };
