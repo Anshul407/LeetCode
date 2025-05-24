0b301c1108a05a3a2c743d8270526764add84f8b
Anshul407 <kunwaranshul7739@gmail.com>
2025-05-24 21:28:53 +0530

    Time: 0 ms (100%), Space: 33 MB (56.49%) - LeetHub

---
+++ 3194-find-words-containing-character/3194-find-words-containing-character.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int>ans;
+        for(int i=0;i<words.size();i++){
+            for(auto cur:words[i]){
+                if(cur==x){
+                    ans.push_back(i);
+                    break;
+                }
+            }
+        }return ans;
+    }
+};
