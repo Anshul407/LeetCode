0a06297dda43b5cdc9c37a0b3922c2fb9daf5509
Anshul407 <kunwaranshul7739@gmail.com>
2025-06-09 11:38:07 +0530

    Time: 0 ms (100%), Space: 11 MB (88.72%) - LeetHub

--- 3540-hash-divided-string/3540-hash-divided-string.cpp
+++ 3540-hash-divided-string/3540-hash-divided-string.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     string stringHash(string s, int k) {
         string ans="";
-        int rem=k;
-        for(int i=0;i<s.length();i+=rem){
-            int cur=0;
-            for(int k=i;k<i+rem;k++){
-                cout<<s[k]<<" ";
-                cur+=(s[k]-'a');
+        int i=0;
+        while(i<s.size()){
+            int cnt=0;
+            int temp=0;
+            while(i<s.size()&&cnt<k){
+                cnt++;
+                temp+=(s[i]-'a');
+                i++;
             }
-            cur%=26;
-            ans+=(cur+'a');
-            cout<<endl;
-        }
-        return ans;
-
+            temp%=26;
+            ans.push_back(temp+'a');
+        }return ans;
     }
 };
