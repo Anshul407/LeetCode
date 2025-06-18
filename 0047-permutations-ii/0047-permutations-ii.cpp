210be1ab95c9dbc94f0626e04d2a0c81ac894a65
Anshul407 <kunwaranshul7739@gmail.com>
2025-06-18 14:15:28 +0530

    Time: 0 ms (100%), Space: 11.4 MB (89.79%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,vector<int>&t,vector<vector<int>>&ans,vector<int>&vis){
-        if(t.size()==nums.size()){
+    void solve(vector<int>&nums,vector<int>&t,vector<vector<int>>&ans,int state){
+        if(state==(1<<nums.size())-1){
             ans.push_back(t);
             return ;
         }
         for(int i=0;i<nums.size();i++){
-            if(i!=0&&nums[i]==nums[i-1]&&!vis[i-1])continue;
-            if(vis[i]==0){
-                vis[i]=1;
+ 
+            int newstate=state|(1<<i);
+            int curstate=state&(1<<i);
+            if(i!=0&&nums[i]==nums[i-1]&&!(state&(1<<(i-1))))continue;
+
+            if(curstate==0){
+               
                 t.push_back(nums[i]);
-                solve(nums,t,ans,vis);
-                vis[i]=0;
+                solve(nums,t,ans,newstate);
                 t.pop_back();
             }
         }
@@ -22,7 +25,7 @@ public:
         vector<int>t;
        vector<int>vis(nums.size(),0);
         vector<vector<int>>ans;
-        solve(nums,t,ans,vis);
+        solve(nums,t,ans,0);
         return ans;
     }
 };
